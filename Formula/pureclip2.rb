6e1eef865a991c62f95eec193a330e44263cd7a8
Johannes Stephan <work@johannesstephan.com>
2026-06-01 18:08:21 +0200

    fix: use real formula from packaging (v2.0.0, boost/gsl/libomp)

--- Formula/pureclip2.rb
+++ Formula/pureclip2.rb
@@ -1,18 +1,26 @@
 class Pureclip2 < Formula
-  desc "Capturing protein-RNA interaction footprints from single-nucleotide CLIP-seq data"
+  desc "Detect protein-RNA interaction footprints from single-nucleotide CLIP-seq data"
   homepage "https://github.com/johan-stph/PureCLIP"
-  url "https://github.com/johan-stph/PureCLIP/archive/refs/tags/v1.3.1.tar.gz"
-  sha256 "" # TODO: fill in after confirming release tarball sha256
-  license "GPL-3.0-or-later"
+  url "https://github.com/johan-stph/PureCLIP/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "4583a4b6061264955c5d5194562979c6fa5f7301f224a7718eae66c69b2c0531"
+  license "MIT"
 
   depends_on "cmake" => :build
-  depends_on "seqan"
+  depends_on "boost"
+  depends_on "gsl"
+  depends_on "libomp"
 
   def install
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "make", "install"
+      system "cmake", "../src",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DGSL_ROOT_DIR=#{Formula["gsl"].opt_prefix}",
+        "-DCMAKE_PREFIX_PATH=#{Formula["boost"].opt_prefix}",
+        *std_cmake_args
+      system "make", "-j#{ENV.make_jobs}"
     end
+    bin.install "build/pureclip"
+    bin.install "build/winextract"
   end
 
   test do
